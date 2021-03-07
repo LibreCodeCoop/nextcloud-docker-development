60777860548e350d19c9a5ad2ad0f30c38488972
Vitor Mattos <vitor@php.rio>
2021-03-07 18:55:25 -0300

    Optimize git clone

--- .docker/app/entrypoint.sh
+++ .docker/app/entrypoint.sh
@@ -3,11 +3,8 @@ cd ../
 . `pwd`/.env
 php .docker/app/wait-for-db.php
 if [ ! -d "nextcloud/.git" ]; then
-    git clone https://github.com/nextcloud/server --branch "${VERSION}" nextcloud
-    cd nextcloud
-    git submodule update --init
-    mkdir data
-    cd -
+    git clone --progress --single-branch --depth 1 --branch "${VERSION}" --recurse-submodules -j 4 https://github.com/nextcloud/server nextcloud
+    mkdir nextcloud/data
 fi
 cd nextcloud
 php -S 0.0.0.0:"$HTTP_PORT"
