bfe866ed3e19280b8eb6a4a48367ca5cec769321
Vitor Mattos <vitor@php.rio>
2021-09-11 14:03:46 -0300

    Add default_phone_region environment

--- .docker/app/entrypoint.sh
+++ .docker/app/entrypoint.sh
@@ -7,5 +7,6 @@ if [ ! -d ".git" ]; then
     mkdir data
     chown -R www-data:www-data .
     runuser -u www-data -- php occ maintenance:install --verbose --database=pgsql --database-name=nextcloud_main --database-host=postgres --database-port= --database-user=nextcloud --database-pass=nextcloud --admin-user=${NEXTCLOUD_ADMIN_USER} --admin-pass=${NEXTCLOUD_ADMIN_PASSWORD}
+    runuser -u www-data -- php occ config:system:set default_phone_region ${DEFAULT_PHONE_REGION}
 fi
 php-fpm
