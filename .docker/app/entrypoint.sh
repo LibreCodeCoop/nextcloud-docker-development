4c41dea5ccd3a61d7eee0939aec4eaec2bb7e82f
Vitor Mattos <vitor@php.rio>
2021-09-25 13:39:12 -0300

    Enable setup using MySQL

--- .docker/app/entrypoint.sh
+++ .docker/app/entrypoint.sh
@@ -6,7 +6,13 @@ if [ ! -d ".git" ]; then
     rsync -r /tmp/nextcloud/ .
     mkdir data
     chown -R www-data:www-data .
-    runuser -u www-data -- php occ maintenance:install --verbose --database=pgsql --database-name=nextcloud_main --database-host=postgres --database-port= --database-user=nextcloud --database-pass=nextcloud --admin-user=${NEXTCLOUD_ADMIN_USER} --admin-pass=${NEXTCLOUD_ADMIN_PASSWORD}
+fi
+if [[ ! -f "config/config.php" && ${AUTOINSTALL} -eq 1 ]]; then
+    if [ ! -z "${POSTGRES_HOST}"]; then
+        runuser -u www-data -- php occ maintenance:install --verbose --database=pgsql --database-name=${POSTGRES_DB} --database-host=${POSTGRES_HOST} --database-port= --database-user=${POSTGRES_USER} --database-pass=${POSTGRES_PASSWORD} --admin-user=${NEXTCLOUD_ADMIN_USER} --admin-pass=${NEXTCLOUD_ADMIN_PASSWORD} --admin-email=${NEXTCLOUD_ADMIN_EMAIL}
+    elif [ ! -z "${MYSQL_HOST}"]; then
+        runuser -u www-data -- php occ maintenance:install --verbose --database=mysql --database-name=${POSTGRES_DB} --database-host=${POSTGRES_HOST} --database-port= --database-user=${POSTGRES_USER} --database-pass=${POSTGRES_PASSWORD} --admin-user=${NEXTCLOUD_ADMIN_USER} --admin-pass=${NEXTCLOUD_ADMIN_PASSWORD} --admin-email=${NEXTCLOUD_ADMIN_EMAIL}
+    fi
     runuser -u www-data -- php occ config:system:set default_phone_region ${DEFAULT_PHONE_REGION}
 fi
 php-fpm
