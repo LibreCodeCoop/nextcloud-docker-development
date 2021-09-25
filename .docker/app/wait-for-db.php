0b3e5d9b166a7f6639b67de0b5afb11d09191113
Vitor Mattos <vitor@php.rio>
2021-09-25 13:36:26 -0300

    Support to MySQL

--- .docker/app/wait-for-db.php
+++ .docker/app/wait-for-db.php
@@ -2,9 +2,15 @@
 <?php
 function dbIsUp() {
     try {
-        if ( !empty('POSTGRES_DB') && !empty('POSTGRES_USER') && !empty('POSTGRES_PASSWORD') && !empty('POSTGRES_HOST') ) {
+        $dsn = 'mysql:dbname='.getenv('MYSQL_DATABASE').';host='.getenv('MYSQL_HOST');
+        if ( getenv('POSTGRES_DB') && getenv('POSTGRES_USER') && getenv('POSTGRES_PASSWORD') && getenv('POSTGRES_HOST') ) {
             $dsn = 'pgsql:dbname='.getenv('POSTGRES_DB').';host='.getenv('POSTGRES_HOST');
+            echo "Connecting to Postgres...\n";
             new PDO($dsn, getenv('POSTGRES_USER'), getenv('POSTGRES_PASSWORD'));
+        } elseif ( getenv('MYSQL_DATABASE') && getenv('MYSQL_USER') && getenv('MYSQL_PASSWORD') && getenv('MYSQL_HOST') ) {
+            echo "Connecting to MySQL...\n";
+            $dsn = 'mysql:dbname='.getenv('MYSQL_DATABASE').';host='.getenv('MYSQL_HOST');
+            new PDO($dsn, getenv('MYSQL_USER'), getenv('MYSQL_PASSWORD'));
         }
     } catch(Exception $e) {
         echo $e->getMessage()."\n";
