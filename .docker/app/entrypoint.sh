88c321f02ea9733f8f0bb5ebc3af64f86752e86f
Vitor Mattos <vitor@php.rio>
2021-09-25 13:52:52 -0300

    Make more tests to check by database settings

--- .docker/app/entrypoint.sh
+++ .docker/app/entrypoint.sh
@@ -8,10 +8,10 @@ if [ ! -d ".git" ]; then
     chown -R www-data:www-data .
 fi
 if [[ ! -f "config/config.php" && ${AUTOINSTALL} -eq 1 ]]; then
-    if [ ! -z "${POSTGRES_HOST}"]; then
+    if [[ ! -z ${POSTGRES_HOST} && ! -z ${POSTGRES_DB} && ! -z ${POSTGRES_USER} && ! -z ${POSTGRES_PASSWORD} ]]; then
         runuser -u www-data -- php occ maintenance:install --verbose --database=pgsql --database-name=${POSTGRES_DB} --database-host=${POSTGRES_HOST} --database-port= --database-user=${POSTGRES_USER} --database-pass=${POSTGRES_PASSWORD} --admin-user=${NEXTCLOUD_ADMIN_USER} --admin-pass=${NEXTCLOUD_ADMIN_PASSWORD} --admin-email=${NEXTCLOUD_ADMIN_EMAIL}
-    elif [ ! -z "${MYSQL_HOST}"]; then
-        runuser -u www-data -- php occ maintenance:install --verbose --database=mysql --database-name=${POSTGRES_DB} --database-host=${POSTGRES_HOST} --database-port= --database-user=${POSTGRES_USER} --database-pass=${POSTGRES_PASSWORD} --admin-user=${NEXTCLOUD_ADMIN_USER} --admin-pass=${NEXTCLOUD_ADMIN_PASSWORD} --admin-email=${NEXTCLOUD_ADMIN_EMAIL}
+    elif [[ ! -z ${MYSQL_HOST} && ! -z ${MYSQL_DATABASE} && ! -z ${MYSQL_USER} && ! -z ${MYSQL_PASSWORD} ]]; then
+        runuser -u www-data -- php occ maintenance:install --verbose --database=mysql --database-name=${MYSQL_DATABASE} --database-host=${MYSQL_HOST} --database-port= --database-user=${MYSQL_USER} --database-pass=${MYSQL_PASSWORD} --admin-user=${NEXTCLOUD_ADMIN_USER} --admin-pass=${NEXTCLOUD_ADMIN_PASSWORD} --admin-email=${NEXTCLOUD_ADMIN_EMAIL}
     fi
     runuser -u www-data -- php occ config:system:set default_phone_region ${DEFAULT_PHONE_REGION}
 fi
