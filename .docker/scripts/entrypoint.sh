d1d30358f4e99cea76cf8cc6dfd4f8f1b8e42193
Vitor Mattos <vitor@php.rio>
2023-06-05 01:24:36 -0300

    Add quotes to prevent error when the content of var have space
    Signed-off-by: Vitor Mattos <vitor@php.rio>

--- .docker/scripts/entrypoint.sh
+++ .docker/scripts/entrypoint.sh
@@ -22,12 +22,12 @@ php /var/www/scripts/wait-for-db.php
 
 # Set configurations, if needed
 if [[ ! -f "config/config.php" && ${AUTOINSTALL} -eq 1 ]]; then
-    if [[ ${DB_HOST} === 'mysql' ]]; then
-        runuser -u www-data -- php occ maintenance:install --verbose --database=${DB_HOST} --database-name=${MYSQL_DATABASE} --database-host=${DB_HOST} --database-port= --database-user=${MYSQL_USER} --database-pass=${MYSQL_PASSWORD} --admin-user=${NEXTCLOUD_ADMIN_USER} --admin-pass=${NEXTCLOUD_ADMIN_PASSWORD} --admin-email=${NEXTCLOUD_ADMIN_EMAIL}
-    elif [[ ${DB_HOST} === 'pgsql' ]]; then
-        runuser -u www-data -- php occ maintenance:install --verbose --database=${DB_HOST} --database-name=${POSTGRES_DB} --database-host=${DB_HOST} --database-port= --database-user=${POSTGRES_USER} --database-pass=${POSTGRES_PASSWORD} --admin-user=${NEXTCLOUD_ADMIN_USER} --admin-pass=${NEXTCLOUD_ADMIN_PASSWORD} --admin-email=${NEXTCLOUD_ADMIN_EMAIL}
+    if [[ ${DB_HOST} == 'mysql' ]]; then
+        runuser -u www-data -- php occ maintenance:install --verbose --database="${DB_HOST}" --database-name="${MYSQL_DATABASE}" --database-host="${DB_HOST}" --database-port= --database-user="${MYSQL_USER}" --database-pass="${MYSQL_PASSWORD}" --admin-user="${NEXTCLOUD_ADMIN_USER}" --admin-pass="${NEXTCLOUD_ADMIN_PASSWORD}" --admin-email="${NEXTCLOUD_ADMIN_EMAIL}"
+    elif [[ "${DB_HOST}" == 'pgsql' ]]; then
+        runuser -u www-data -- php occ maintenance:install --verbose --database="${DB_HOST}" --database-name="${POSTGRES_DB}" --database-host="${DB_HOST}" --database-port= --database-user="${POSTGRES_USER}" --database-pass="${POSTGRES_PASSWORD}" --admin-user="${NEXTCLOUD_ADMIN_USER}" --admin-pass="${NEXTCLOUD_ADMIN_PASSWORD}" --admin-email="${NEXTCLOUD_ADMIN_EMAIL}"
     else
-        runuser -u www-data -- php occ maintenance:install --verbose --admin-user=${NEXTCLOUD_ADMIN_USER} --admin-pass=${NEXTCLOUD_ADMIN_PASSWORD} --admin-email=${NEXTCLOUD_ADMIN_EMAIL}
+        runuser -u www-data -- php occ maintenance:install --verbose --admin-user="${NEXTCLOUD_ADMIN_USER}" --admin-pass="${NEXTCLOUD_ADMIN_PASSWORD}" --admin-email=${NEXTCLOUD_ADMIN_EMAIL}
     fi
 
     runuser -u www-data -- php occ config:import <<EOF
@@ -62,13 +62,13 @@ EOF
     runuser -u www-data -- php occ config:system:set loglevel                   --value 0 --type integer
     runuser -u www-data -- php occ config:system:set query_log_file             --value /var/www/html/data/database.log
 
-    runuser -u www-data -- php occ config:system:set default_phone_region       --value ${DEFAULT_PHONE_REGION}
+    runuser -u www-data -- php occ config:system:set default_phone_region       --value "${DEFAULT_PHONE_REGION}"
     runuser -u www-data -- php occ config:system:set allow_local_remote_servers --value true --type boolean
 
-    runuser -u www-data -- php occ config:system:set mail_from_address          --value ${MAIL_FROM_ADDRESS}
-    runuser -u www-data -- php occ config:system:set mail_domain                --value ${MAIL_DOMAIN}
+    runuser -u www-data -- php occ config:system:set mail_from_address          --value "${MAIL_FROM_ADDRESS}"
+    runuser -u www-data -- php occ config:system:set mail_domain                --value "${MAIL_DOMAIN}"
     runuser -u www-data -- php occ config:system:set mail_smtpport              --value ${MAIL_SMTPPORT} --type integer
-    runuser -u www-data -- php occ config:system:set mail_smtphost              --value ${MAIL_SMTPHOST}
+    runuser -u www-data -- php occ config:system:set mail_smtphost              --value "${MAIL_SMTPHOST}"
 
     runuser -u www-data -- php occ config:app:set core backgroundjobs_mode      --value "cron"
 fi
